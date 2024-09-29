4de6dd5d7132c117a17554a02c00905fd8466c1f
Virendra3010 <virupatel9264@gmail.com>
2024-09-29 07:38:49 +0530

    change password option if user forget it.

---
+++ landingPage/script.js
@@ -0,0 +1,7 @@
+document.getElementById('forgot-password').addEventListener('click', function() {
+    alert('Redirecting to the password reset page...');
+});
+
+document.getElementById('existing-user').addEventListener('click', function() {
+    alert('Redirecting to the sign-in page...');
+});
